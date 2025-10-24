1fbdff83c367c29a09c1468c4209a646424f6ddc
alataq.tnt <alataq.tnt@gmail.com>
2025-10-24 13:02:40 +0000

    Adding emulator route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import index from "./pages/index.html";
+import emulator from "./pages/emulator.html";
 
 Bun.serve({
     port: 3000,
     routes: {
+        "/emulator": emulator,
         "/": index,
     }
 })
