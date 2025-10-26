dbbdb7fc0006dfc2557bd9276791e3d3121f044d
alataq.tnt <alataq.tnt@gmail.com>
2025-10-26 03:08:37 +0000

    Making first operations

--- src/emulator/components/alu.ts
+++ src/emulator/components/alu.ts
@@ -1,3 +1,4 @@
+import type { Register } from "./cpu";
 import { type Machine } from "./machine";
 
 export class ALU {
@@ -8,6 +9,23 @@ export class ALU {
     }
     
     public step(){
-        
+        let cp = this.machine.cpu.registers["cp"] as Register;
+
+        const OPERATION_ADDRESS = cp.read();
+        const OPERATION = this.machine.memory.read16(OPERATION_ADDRESS) as number;
+        const OPERATION_CODE = OPERATION >> 12;
+
+        switch(OPERATION_CODE) {
+            case 0: {
+                cp.write(OPERATION_ADDRESS + 2);
+                break;
+            }
+            case 1: {
+                const SUB_OPERATION_CODE = (OPERATION >> 9) & 0x7;
+                
+            }
+        }
+
+
     }
 }
