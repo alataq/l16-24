511968bc0ad6f3e5f170b11f2a7db2ad0b3596a7
alataq.tnt <alataq.tnt@gmail.com>
2025-10-26 01:29:16 +0000

    Making the base for the alu

---
+++ src/emulator/components/alu.ts
@@ -0,0 +1,13 @@
+import { type Machine } from "./machine";
+
+export class ALU {
+    private machine: Machine;
+    
+    constructor(machine: Machine) {
+        this.machine = machine;
+    }
+    
+    public step(){
+        
+    }
+}
