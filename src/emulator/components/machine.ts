a3949b7dc022bcd71e2d98a05aed85f79c40b49d
alataq.tnt <alataq.tnt@gmail.com>
2025-10-26 01:30:11 +0000

    Linking the alu

--- src/emulator/components/machine.ts
+++ src/emulator/components/machine.ts
@@ -1,12 +1,15 @@
 import { Memory } from "./memory";
 import { CPU } from "./cpu";
+import { ALU } from "./alu";
 
 export class Machine {
     public memory: Memory;
     public cpu: CPU;
+    public alu: ALU
 
     constructor(memorySize: number){
         this.memory = new Memory(memorySize);
         this.cpu = new CPU();
+        this.alu = new ALU(this);
     }
 }
